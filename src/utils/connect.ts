12eced17425ff2726f7755d0a1680ca0fdfeaed6
Trend20 <enockomondi305@gmail.com>
2022-11-15 00:03:17 +0300

    application to the database

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
-import config from 'config';
+import config from "config";
 
-const connect = async () =>{
-  const dbUrl = config.get<string>('dbUrl');
-
-  return await mongoose.connect(dbUrl)
-                  .then(() =>{
-                    console.log('Connected to the database successfully!')
-                  })
-                  .catch((error) =>{
-                    console.error(error)
-                    process.exit(1)
-                  })
-}
+const connect = async () => {
+  const dbUrl = config.get<string>("dbUrl");
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("Connected to the database successfully!");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 export default connect;
